48bee5065af90d158401c356e7d66359fd3a9590
Jason Hung <jason.hung@tomofun.com>
2023-02-10 12:51:45 +0800

    Create NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const NotFoundPage = () => <div>NotFoundPage</div>;
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/heroes');
+    }, 1000);
+  }, []);
+
+  return <h1>404 Not Found</h1>;
+};
 
 export default NotFoundPage;
