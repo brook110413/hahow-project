95b857ff37783c18d6d78703ed66982790c346c1
Jason Hung <jason.hung@tomofun.com>
2023-02-12 13:20:24 +0800

    Update i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
 import { CssBaseline, Container } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '@/styles/theme';
 import store from '@/redux/store';
-import i18n from '@/utils/i18n';
 import router from '@/configs/routers';
 import sxStyle from './app.style';
+import '@/utils/i18n';
 
 const App = () => (
-  <Provider store={store} i18n={i18n}>
-    <I18nextProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container sx={sxStyle.container}>
-          <RouterProvider router={router} />
-        </Container>
-      </ThemeProvider>
-    </I18nextProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container sx={sxStyle.container}>
+        <RouterProvider router={router} />
+      </Container>
+    </ThemeProvider>
   </Provider>
 );
 
